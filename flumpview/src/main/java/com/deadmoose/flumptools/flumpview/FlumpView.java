eaa7a316ed5a77009650c220aaeaed734b56b331
David Hoover <karma@deadmoose.com>
2013-09-18 11:23:30 -0700

    Crack-tastic hacked together PlayN-based flump viewer

--- flumpview/src/main/java/com/deadmoose/flumptools/flumpview/FlumpView.java
+++ flumpview/src/main/java/com/deadmoose/flumptools/flumpview/FlumpView.java
@@ -1,10 +1,21 @@
 package com.deadmoose.flumptools.flumpview;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 import playn.core.Game;
 import playn.core.PlayN;
+import playn.core.gl.Scale;
 import playn.core.util.Clock;
-
+import playn.java.JavaAssets;
 import playn.java.JavaPlatform;
+
+import tripleplay.game.ScreenStack;
 
 public class FlumpView extends Game.Default
 {
@@ -13,7 +24,51 @@ public class FlumpView extends Game.Default
     public static void main (String[] args)
     {
         JavaPlatform.Config config = new JavaPlatform.Config();
+        config.width = 1280;
+        config.height = 1024;
         JavaPlatform.register(config);
+
+        JavaPlatform platform = new JavaPlatform(config) {
+            @Override public JavaAssets assets () {
+                return _assets;
+            }
+            protected final JavaAssets _assets = new JavaAssets(this) {
+                @Override protected URL requireResource (String path) throws FileNotFoundException {
+                    if (path.startsWith("/")) {
+                        URL url = null;
+                        try {
+                            url = new File(path).toURI().toURL();
+                        } catch (MalformedURLException mue) {
+                            PlayN.log().error("Error loading file: " + path, mue);
+                        }
+
+                        if (url == null) {
+                            throw new FileNotFoundException(path);
+                        }
+
+                        return url;
+                    } else {
+                        return super.requireResource(path);
+                    }
+                }
+
+                @Override protected Scale assetScale () {
+                    return _scale;
+                }
+
+                protected Scale _scale = new Scale(1) {
+                    @Override public List<Scale.ScaledResource> getScaledResources(String path) {
+                        return Lists.newArrayList(new Scale.ScaledResource(this, path));
+
+                    }
+                };
+
+                {
+                    setPathPrefix("");
+                }
+            };
+        };
+        PlayN.setPlatform(platform);
         PlayN.run(new FlumpView());
     }
 
@@ -24,18 +79,32 @@ public class FlumpView extends Game.Default
 
     @Override public void init()
     {
+        _screens.push(new FlumpViewScreen(_screens));
     }
 
     @Override public void update (int delta)
     {
         _clock.update(delta);
+        _screens.update(delta);
     }
 
     @Override public void paint (float alpha)
     {
         _clock.paint(alpha);
+        _screens.paint(_clock);
     }
 
     protected final Clock.Source _clock = new Clock.Source(UPDATE_RATE);
+    protected final ScreenStack _screens = new ScreenStack() {
+        @Override protected void handleError (RuntimeException error) {
+            PlayN.log().warn("Screen failure", error);
+        }
+        @Override protected Transition defaultPushTransition () {
+            return slide();
+        }
+        @Override protected Transition defaultPopTransition () {
+            return slide().right();
+        }
+    };
 }
 
---
+++ flumpview/src/main/java/com/deadmoose/flumptools/flumpview/FlumpViewScreen.java
@@ -0,0 +1,157 @@
+package com.deadmoose.flumptools.flumpview;
+
+import java.io.File;
+import java.util.Map;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import playn.core.GroupLayer;
+import playn.core.ImmediateLayer;
+import playn.core.PlayN;
+import playn.core.Surface;
+import playn.core.util.Callback;
+import playn.core.util.Clock;
+
+import react.UnitSlot;
+import react.Value;
+
+import tripleplay.flump.Library;
+import tripleplay.flump.Movie;
+import tripleplay.flump.MoviePlayer;
+import tripleplay.flump.Symbol;
+import tripleplay.game.ScreenStack;
+import tripleplay.game.UIScreen;
+import tripleplay.ui.Button;
+import tripleplay.ui.Group;
+import tripleplay.ui.Root;
+import tripleplay.ui.Scroller;
+import tripleplay.ui.Shim;
+import tripleplay.ui.SimpleStyles;
+import tripleplay.ui.layout.AxisLayout;
+import tripleplay.ui.layout.BorderLayout;
+import tripleplay.util.Colors;
+
+public class FlumpViewScreen extends UIScreen
+{
+    public final Value<Library> library = Value.create(null);
+
+    public FlumpViewScreen (ScreenStack screens)
+    {
+        _screens = screens;
+    }
+
+    @Override public void wasShown ()
+    {
+        super.wasShown();
+
+        _root = iface.createRoot(new BorderLayout(), SimpleStyles.newSheet(), layer);
+        _root.setSize(width(), height());
+
+        Shim shim = new Shim(1, 1) {
+            @Override protected void layout () {
+                super.layout();
+
+                _flumpLayer.setTranslation(size().width()/2, size().height()/2);
+            }
+        };
+        shim.layer.add(_flumpLayer);
+        _root.add(shim.setConstraint(BorderLayout.CENTER));
+
+        _flumpLayer.add(PlayN.graphics().createImmediateLayer(new ImmediateLayer.Renderer() {
+            public void render (Surface surface) {
+                surface.setFillColor(Colors.WHITE);
+                surface.fillRect(-10, -1, 20, 2);
+                surface.fillRect(-1, -10, 2, 20);
+            }
+        }).setDepth(1));
+
+        Button loadButton = new Button("Load Library");
+        loadButton.clicked().connect(new UnitSlot() {
+            @Override public void onEmit () {
+                openFileChooser();
+            }
+        });
+        _root.add(loadButton.setConstraint(BorderLayout.SOUTH));
+
+        _movies = new Group(AxisLayout.vertical());
+
+        _root.add(new Scroller(_movies).setConstraint(BorderLayout.EAST));
+
+        openFileChooser();
+    }
+
+    @Override public void wasHidden ()
+    {
+        super.wasHidden();
+
+        iface.destroyRoot(_root);
+    }
+
+    @Override public void paint (Clock clock) {
+        super.paint(clock);
+        if (_player != null) {
+            _player.paint(clock);
+        }
+    }
+
+    protected void openFileChooser ()
+    {
+        if (_chooser != null) {
+            _chooser.cancelSelection();
+        }
+
+        _chooser = new JFileChooser();
+        _chooser.setFileFilter(new FileNameExtensionFilter("Flump libraries", "json"));
+
+        int returnVal = _chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            loadFlumpLibrary(_chooser.getSelectedFile());
+        }
+    }
+
+    protected void loadFlumpLibrary (File file)
+    {
+        Library.fromAssets(file.getParentFile().getAbsolutePath(), new Callback<Library>() {
+            public void onSuccess (Library library) {
+                if (_player != null) {
+                    _player.destroy();
+                }
+
+                _player = new MoviePlayer(library);
+                _flumpLayer.add(_player.layer());
+
+                _movies.destroyAll();
+
+                for (final Map.Entry<String, Symbol> entry : library.symbols.entrySet()) {
+                    if (entry.getKey().startsWith("~")) {
+                        continue;
+                    }
+                    if (entry.getValue() instanceof Movie.Symbol) {
+                        Button button = new Button(entry.getKey());
+                        button.onClick(new UnitSlot() {
+                            @Override public void onEmit () {
+                                _player.loop(entry.getKey());
+                            }
+                        });
+
+                        _movies.add(button);
+                    }
+                }
+
+                ((Button)_movies.childAt(0)).click();
+            }
+
+            public void onFailure (Throwable cause) {
+                PlayN.log().error("Error loading flump", cause);
+            }
+        });
+    }
+
+    protected Root _root;
+    protected ScreenStack _screens;
+    protected JFileChooser _chooser;
+    protected MoviePlayer _player;
+    protected GroupLayer _flumpLayer = PlayN.graphics().createGroupLayer();
+    protected Group _movies;
+}
